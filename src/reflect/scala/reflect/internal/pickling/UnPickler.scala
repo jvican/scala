4bc626ca898fa88a4844be31bf20e95254d13ff2
jvican <jorgevc@fastmail.es>
2016-12-22 13:06:57 +0100

    Don't use owner's position, pass it explicitly
    Setting the `symbolOnCompletion` as the owner of the stub is dangerous and could produce silent misbehaviours in the compiler. Instead, pass the only bit of the `symbolOnCompletion` we are interested in (the position) explicitly.

--- src/reflect/scala/reflect/internal/pickling/UnPickler.scala
+++ src/reflect/scala/reflect/internal/pickling/UnPickler.scala
@@ -267,12 +267,13 @@ abstract class UnPickler {
                 // (5) Create a stub symbol to defer hard failure a little longer.
                 val advice = moduleAdvice(s"${owner.fullName}.$name")
                 val missingMessage =
-                  s"""|${symbolTable.completingSymbol}missing or invalid dependency detected while loading class file '$filename'.
+                  s"""|missing or invalid dependency detected while loading class file '$filename'.
                       |Could not access ${name.longString} in ${owner.kindString} ${owner.fullName},
                       |because it (or its dependencies) are missing. Check your build definition for
                       |missing or conflicting dependencies. (Re-run with `-Ylog-classpath` to see the problematic classpath.)
                       |A full rebuild may help if '$filename' was compiled against an incompatible version of ${owner.fullName}.$advice""".stripMargin
-                symbolTable.symbolOnCompletion.newStubSymbol(name, missingMessage)
+                val atPos = symbolTable.symbolOnCompletion.pos
+                NoSymbol.newStubSymbol(name, missingMessage, atPos)
               }
             }
           }
@@ -398,7 +399,7 @@ abstract class UnPickler {
         val sym = readSymbolRef() match {
           case stub: StubSymbol if !stub.isClass =>
             // SI-8502 This allows us to create a stub for a unpickled reference to `missingPackage.Foo`.
-            stub.owner.newStubSymbol(stub.name.toTypeName, stub.missingMessage, isPackage = true)
+            stub.owner.newStubSymbol(stub.name.toTypeName, stub.missingMessage, stub.owner.pos, isPackage = true)
           case sym => sym
         }
         ThisType(sym)
